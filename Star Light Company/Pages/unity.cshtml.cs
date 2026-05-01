168b2279c2b6060e12d64e647c5393b80e8262f9
Anh Hào Nguyễn <anhhaonguyen27032013@outlook.com>
2026-05-01 21:12:32 +0700

    New Page

---
+++ Star Light Company/Pages/unity.cshtml.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Star_Light_Company.Pages
+{
+
+    public class unityModel : PageModel
+    {
+        private readonly IWebHostEnvironment _env;
+        public List<string> assets = new List<string>();
+        public string? folder;
+
+        public unityModel(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+        public void OnGet()
+        {
+            string folderPath = Path.Combine(_env.WebRootPath, "assets");
+            folder = folderPath;
+
+            // 2. Kiểm tra nếu thư mục tồn tại để tránh lỗi gỡ lỗi
+            if (Directory.Exists(folderPath))
+            {
+                // 3. Lấy danh sách tên file (ví dụ lọc file .unitypackage)
+                var files = Directory.GetFiles(folderPath)
+                                     .Select(Path.GetFileName)
+                                     .ToList();
+                assets = files!;
+            }
+        }
+        public string GetFileUrl(string asset)
+        {
+            string path = Path.Combine(folder!, asset);
+            return path;
+        }
+    }
+}
